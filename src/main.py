75ca9e34b27467e6a8aa856eff4f737b7a503957
abarolli <obarolli@gmail.com>
2025-01-28 16:55:12 -0700

    Added scan_jobs and scan_builds routines

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 
 from argparse import ArgumentParser, Namespace
 import os
-import traceback
 
 import requests
 import requests.auth
@@ -23,15 +22,49 @@ def collect_input() -> Namespace:
     return args
 
 
-def scan_jobs(jobs: list[dict]):
-    pass
+def scan_jobs(jobs: list[dict], search_string: str):
+    
+    for job in jobs:
+        job_api_url = job.get('url') + '/api/json'
+        res = requests.get(
+            job_api_url,
+            auth=requests.auth.HTTPBasicAuth(os.environ.get('JENKINS_USER'), os.environ.get('JENKINS_PASSWORD'))
+        )
+        
+        if not res.ok:
+            print('uh oh')
+
+        job_data = res.json()
+
+        if (more_jobs := job_data.get('jobs')):
+            scan_jobs(more_jobs, search_string)
+        elif (builds := job_data.get('builds')):
+            scan_builds(builds, search_string)
 
 
-def scan_builds(builds: list[dict]):
-    pass
+def scan_builds(builds: list[dict], search_string: str):
+    
+    for build in builds:
+        build_log_url = build.get('url') + 'consoleText'
+        res = requests.get(
+            build_log_url,
+            auth=requests.auth.HTTPBasicAuth(os.environ.get('JENKINS_USER'), os.environ.get('JENKINS_PASSWORD'))
+        )
+
+        if not res.ok:
+            raise requests.exceptions.RequestException(
+                f'Error requesting url {build_log_url}; is this a valid Jenkins url?',
+                request=res.request,
+                response=res)
+        
+        build_log_contents = res.text
+        if search_string in build_log_contents:
+            print('Found search string')
+        else:
+            print('Did not find the search string')
 
 
-def scan_jenkins(url: str) -> None:
+def scan_jenkins(url: str, search_string: str) -> None:
     
     api_url = url + '/api/json'
     res = requests.get(
@@ -47,15 +80,15 @@ def scan_jenkins(url: str) -> None:
     
     jenkins_data = res.json()
     if (jobs := jenkins_data.get('jobs')):
-        scan_jobs(jobs)
+        scan_jobs(jobs, search_string)
     elif (builds := jenkins_data.get('builds')):
-        scan_builds(builds)
+        scan_builds(builds, search_string)
 
 
 def main():
 
     args = collect_input()
-    scan_jenkins(args.jenkins_url)
+    scan_jenkins(args.jenkins_url, args.search_string)
 
 
 if __name__ == '__main__':
