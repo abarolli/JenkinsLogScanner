b85e420c84aece03704be39407d27836de68b01a
abarolli <obarolli@gmail.com>
2025-01-28 18:35:03 -0700

    Added BuildScan and Operation classes

--- src/scan_jenkins.py
+++ src/scan_jenkins.py
@@ -1,7 +1,56 @@
 import os
+from typing import List
 
 import requests
 import requests.auth
+
+
+class BuildScan:
+    def __init__(self, jobUrl: str, buildNumber: int):
+        self.__jobUrl = jobUrl
+        self.__buildNumber = buildNumber
+        self.__results = {}
+
+
+    @property
+    def jobUrl(self): return self.__jobUrl
+
+
+    @property
+    def buildNumber(self): return self.__buildNumber
+
+
+    @property
+    def results(self): return self.__results
+
+
+    def add_result(self, key: str, result):
+        self.__results.update({key: result})
+
+
+    def __str__(self):
+        return str({"jobUrl": self.__jobUrl, "buildNumber": self.__buildNumber, **self.__results})
+
+
+class Operation:
+    '''
+    Represents a callable that's not expected to be called until some unknown time. At initialization, optional `kwargs` can be provided
+    and these will be passed to the callable when the `call` method is invoked at a later time.
+    '''
+    def __init__(self, name: str, f: callable, **kwargs):
+        self.__name = name
+        self.__f = f
+        self.__kwargs = kwargs
+
+
+    @property
+    def name(self): return self.__name
+
+    def call(self, *args, **kwargs):
+        '''
+        Any `kwargs` passed here will override the `kwargs` that were supplied at initialization.
+        '''
+        return self.__f(*args, **self.__kwargs, **kwargs)
 
 
 class JenkinsLogScanner:
@@ -30,8 +79,9 @@ class JenkinsLogScanner:
         return res
 
 
-    def __scan_jobs(self, jobs: list[dict], search_string: str):
+    def __scan_jobs(self, jobs: list[dict], operations: List[Operation]) -> List[BuildScan]:
         
+        results: List[BuildScan] = []
         for job in jobs:
             job_api_url = job.get('url') + '/api/json'
             res = self.__request(job_api_url)
@@ -39,30 +89,39 @@ class JenkinsLogScanner:
             job_data = res.json()
 
             if (more_jobs := job_data.get('jobs')):
-                self.__scan_jobs(more_jobs, search_string)
+                scanned_builds = self.__scan_jobs(more_jobs, operations)
             elif (builds := job_data.get('builds')):
-                self.__scan_builds(builds, search_string)
-
-
-    def __scan_builds(self, builds: list[dict], search_string: str):
+                scanned_builds = self.__scan_builds(builds, operations)
+            
+            results.extend(scanned_builds)
         
+        return results
+
+
+    def __scan_builds(self, builds: list[dict], operations: List[Operation]) -> List[BuildScan]:
+        
+        results: List[BuildScan] = []
         for build in builds:
             build_log_url = build.get('url') + 'consoleText'
             res = self.__request(build_log_url)
             build_log_contents = res.text
-            if search_string in build_log_contents:
-                print('Found search string')
-            else:
-                print('Did not find the search string')
+            build_scan = BuildScan(build.get('url').rsplit('/', 2)[0], build.get('number'))
+            for operation in operations:
+                build_scan.add_result(operation.name, operation.call(build_log_contents))
+            results.append(build_scan)
+
+        return results
 
 
-    def scan_jenkins(self, search_string: str) -> None:
-        
+    def scan_jenkins(self, operations: List[Operation]) -> List[BuildScan]:
+        '''
+        Returns a list of BuildScan objects.
+        '''
         res = self.__request(self.__url + '/api/json')
         
         jenkins_data = res.json()
-        
+
         if (jobs := jenkins_data.get('jobs')):
-            self.__scan_jobs(jobs, search_string)
+            return self.__scan_jobs(jobs, operations)
         elif (builds := jenkins_data.get('builds')):
-            self.__scan_builds(builds, search_string)
+            return self.__scan_builds(builds, operations)
