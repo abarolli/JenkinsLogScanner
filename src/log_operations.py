27b1e3ae79f052737cdafb4c72201e4925d1d26a
abarolli <obarolli@gmail.com>
2025-01-28 21:48:58 -0700

    Added validation logic to head and tail

--- src/log_operations.py
+++ src/log_operations.py
@@ -15,11 +15,17 @@ def build_agent(build_log: str):
 
 def head(build_log: str, lineCount: int = 1) -> str:
     '''Grabs the first `lineCount` number of lines in the `build_log`'''
+    if lineCount <= 0:
+        raise ValueError('lineCount must be larger than 0')
+        
     lines = build_log.splitlines(keepends=1)
     return ''.join(lines[:min(len(lines), lineCount)])
 
 
 def tail(build_log: str, lineCount: int = 1) -> str:
     '''Grabs the last `lineCount` number of lines in the `build_log`'''
+    if lineCount <= 0:
+        raise ValueError('lineCount must be larger than 0')
+        
     lines = build_log.splitlines(keepends=1)
     return ''.join(lines[-(min(len(lines), lineCount)):])
