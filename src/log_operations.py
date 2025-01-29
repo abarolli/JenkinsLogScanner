156719e134b7296476e0c4c22fc1f9d676e690ac
abarolli <obarolli@gmail.com>
2025-01-28 21:18:22 -0700

    Added log_operations.py

---
+++ src/log_operations.py
@@ -0,0 +1,25 @@
+
+
+import re
+
+
+def build_agent(build_log: str):
+    '''Finds the build agent on a simple build running on a single agent'''
+    pattern = re.compile('Building remotely on (.*?) ')
+    match = pattern.search(build_log)
+    if not match:
+        return "built-in node"
+    
+    return match.group(1)
+
+
+def head(build_log: str, lineCount: int) -> str:
+    '''Grabs the first `lineCount` number of lines in the `build_log`'''
+    lines = build_log.splitlines(keepends=1)
+    return ''.join(lines[:min(len(lines), lineCount)])
+
+
+def tail(build_log: str, lineCount: int) -> str:
+    '''Grabs the last `lineCount` number of lines in the `build_log`'''
+    lines = build_log.splitlines(keepends=1)
+    return ''.join(lines[-(min(len(lines), lineCount)):])
